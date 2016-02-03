f77d0cfd4c9e2da09d89541f902e1338e1a4337b
drexlerj <drexlerj@ELABSB135-14.easternct.edu>
2016-02-03 10:34:44 -0500

    I added a prompt for the students name by creating a string variable 'name' as well as a response.

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main () {
+
+  string name;
   
   cout << "Hello Class!\n\n";
+
+  cout << "Please enter your name!\n\n";
+
+  cin >> name;
+
+  cout << "Hello " << name << ", my name is Jeremy";
 
   return 0;
 }
